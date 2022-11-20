7772a01cb009fc3ac6da5313614f1f5138a31be3
Lassi Maksimainen <lassi.maksimainen@gmail.com>
2022-11-20 13:00:02 +0200

    Added first version of sparkline component implementation

---
+++ Sparkline/README.md
@@ -0,0 +1,7 @@
+# Magx-Sparkline
+
+[Sparkline](https://en.wikipedia.org/wiki/Sparkline) is a very plain and usually smallish chart that is used to show a variation in a measurement. It is used for providing a quick-glance and simple information without extra bells and whistles.
+
+## Examples
+
+## How To Use
---
+++ Sparkline/main.ts
@@ -0,0 +1,34 @@
+import "./src/Sparkline";
+import { MagxSparkline, RefLineType, SparklineType, Filltype, Linetype } from "./src/Sparkline";
+
+const sl = document.getElementById("sparkline") as MagxSparkline;
+
+sl.setType(SparklineType.Line);
+sl.setLineWidth(1.5);
+//sl.setLineColor(Linetype.Solid, { r:75, g:75, b:75, a:0.75 });
+sl.setLineColor(Linetype.AboveOneColBelowOneCol, { above: { r: 0, b: 0, g: 255, a: .5 }, below: { r: 255, b: 0, g: 0, a: .5 } });
+//sl.setFill(Filltype.Solid, { r:150, g:150, b:150, a:0.5 });
+sl.setFill(Filltype.AboveOneColBelowOneCol, { above: {r:0, b:0, g:255, a:1}, below: {r:255, b:0, g:0, a: 1} });
+sl.setFill(Filltype.Gradient, { above: { r: 0, b: 0, g: 255, a: 0.5 }, below: { r: 255, b: 0, g: 0, a: 0.5 } });
+sl.setReferenceLine(RefLineType.Middle);
+sl.setReferenceLineColor({r:75, g:75, b:75, a: 1.0});
+sl.setEndpoint(10.0, { r: 0.0, g:0.0, b:255.0, a:1.0 });
+sl.renderCanvas();
+//sl.setDataPointNum(50);
+sl.setLowerBound(true, -5);
+sl.setUpperBound(true, 5);
+//sl.setCap(true, true);
+
+function randn_bm() {
+  let u = 1 - Math.random();
+  let v = Math.random();
+  return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+}
+
+/*
+setInterval(() => {
+  const c = document.getElementById("sparkline") as MagxSparkline;  
+  const val = randn_bm();  
+  c.addDatapoint(val * 5);   
+  c.renderCanvas();  
+}, 500);*/
